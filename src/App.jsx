433ffee0e8269dbcdd3706ac687df88daa10156d
Philip Gaunitz <fluffen@pgaunitz.se>
2020-03-03 16:15:56 +0100

    added login form, added auth module

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,50 @@
-import React from 'react';
+import React, { Component } from "react";
 
-const App = () => {
+import LoginForm from "./components/LoginForm";
+
+
+class App extends Component {
+
+  state = {
+    renderLoginForm: false,
+    authenticated: false,
+    message: ""
+  }
+
+  render() {
+    const { renderLoginForm, authenticated, message } = this.state;
+    let renderLogin;
+    switch (true) {
+      case renderLoginForm && !authenticated:
+        renderLogin = <LoginForm submitFormHandler={this.onLogin} />;
+        break;
+      case !renderLoginForm && !authenticated:
+        renderLogin = (
+          <>
+            <button
+              id="login"
+              onClick={() => this.setState({ renderLoginForm: true })}
+            >
+              Login
+            </button>
+            <p id='message'>{message}</p>
+          </>
+        );
+        break;
+      case authenticated:
+        renderLogin = (
+          <p id='message'>Hi {JSON.parse(sessionStorage.getItem("credentials")).uid}</p>
+        );
+    }
+  
   return (
     <>
-      <h1>Hello Wörld</h1>
+      
+      {renderLogin}
+        
     </>
   );
+  };
 }
 
 export default App;
