1be9d83962e1c39abaaa25dbbc17b0c82eff1272
pierre-1 <pierreantoine.tiberi@gmail.com>
2020-03-08 16:47:57 +0100

    Initial setup for userCanAddProductToOrder cypress test

---
+++ cypress/integration/userCanAddProductsToOrder.feature.js
@@ -0,0 +1,31 @@
+
+
+describe('User can add products to his/her order', () => {
+
+    before(() => {
+        cy.server();
+        cy.route({
+            method: 'GET',
+            url: 'http://localhost:3000/api/products',
+            response: 'fixture:menu_data.json'
+        })
+
+        cy.route({
+            method: 'POST',
+            url: 'http://localhost:3000/api/orders',
+            response: { message: 'The product has been added to your order'}
+        })
+    });
+
+    it('user gets a confirmation message when adding to order', () => {
+        cy.visit('http://localhost:3001')
+        cy.get('#product-1').within(()=>{
+            cy.get('button').contains('Add to order').click()
+        })
+        cy.wait(500)
+        cy.get('#product-2').within(()=>{
+            cy.get('div .message').should('contain', "The product has been added to your order")
+        })
+    });
+
+});
