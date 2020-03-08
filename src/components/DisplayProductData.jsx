c2ae1568862c6babba8458246b12e855bbed11cd
Philip Gaunitz <fluffen@pgaunitz.se>
2020-03-08 17:23:15 +0100

    added response message to cypress test

--- src/components/DisplayProductData.jsx
+++ src/components/DisplayProductData.jsx
@@ -1,54 +1,63 @@
- 
-import React, { Component } from 'react';
-import { getData } from '../modules/productData';
-import axios from 'axios'
+import React, { Component } from "react";
+import { getData } from "../modules/productData";
+import axios from "axios";
 class DisplayProductData extends Component {
-	state = {
+  state = {
     productData: [],
     message: {}
-	}
+  };
 
-	componentDidMount() {
-		this.getProductData()
-	}
+  componentDidMount() {
+    this.getProductData();
+  }
 
-	async getProductData() {
-		let result = await getData();
-		this.setState({ productData: result.data.products })
-	}
+  async getProductData() {
+    let result = await getData();
+    this.setState({ productData: result.data.products });
+  }
 
-async addToOrder(event) {
-let id = event.target.parentElement.dataset.id
-let result = await axios.post('http://localhost:3000/api/orders', {params: {id: id}})
-this.setState({message: {id: id, message: result.data.message}})
-}
+  async addToOrder(event) {
+    let id = event.target.parentElement.dataset.id;
+    let result = await axios.post("http://localhost:3000/api/orders", {
+      params: { id: id }
+    });
+    this.setState({ message: { id: id, message: result.data.message } });
+  }
 
-	render() {
-		let dataIndex
-		if (Array.isArray(this.state.productData) && this.state.productData.length ) {
-			dataIndex = (
-				<div id="index">
-					{this.state.productData.map(item => {
+  render() {
+    let dataIndex;
+    if (
+      Array.isArray(this.state.productData) &&
+      this.state.productData.length
+    ) {
+      dataIndex = (
+        <div id="index">
+          {this.state.productData.map(item => {
             return (
-            <div key={item.id} id={`product-${item.id}`} data-id={item.id} data-price={item.price}>
-            {item.name}{item.description}{item.price}
-            <button onClick={this.addToOrder.bind(this)}>Add to order </button>
-            {this.state.message.id === item.id && 
-              <p>{this.state.message.message}</p>
-            }
-            </div>
-            )
-					})}
-				</div>
-			)
-		} 
+              <div
+                key={item.id}
+                id={`product-${item.id}`}
+                data-id={item.id}
+                data-price={item.price}
+              >
+                {item.name}
+                {item.description}
+                {item.price}
+                <button onClick={this.addToOrder.bind(this)}>
+                  Add to order
+                </button>
+                {parseInt(this.state.message.id) === item.id && (
+                  <p class='message'>{this.state.message.message}</p>
+                )}
+              </div>
+            );
+          })}
+        </div>
+      );
+    }
 
-		return (
-			<div>
-				{dataIndex}
-			</div>
-		)
-	}
+    return <div>{dataIndex}</div>;
+  }
 }
 
 export default DisplayProductData;
